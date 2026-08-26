a769d213237b8665958f025dd0455788ff3b7bb6
seo712000-dotcom <seo71.2000@gmail.com>
2026-08-26 15:57:52 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="trend-screener-v51-market-classification";
+const CACHE_NAME="trend-screener-v52-kosdaq-fallback";
 const APP_SHELL=["./","index.html","paper-engine.js","manifest.json"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
